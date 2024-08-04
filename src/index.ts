24fcae7da2b15b301e96392e87c7beff88ee015f
coinchimp <pinrojas+crypto@gmail.com>
2024-08-04 20:34:12 +0200

    adding register processor

--- src/index.ts
+++ src/index.ts
@@ -43,34 +43,31 @@ if (!databaseUrl) {
 if (DEBUG) monitoring.debug(`Main: Getting Database URL`);
 
 if (DEBUG) monitoring.debug(`Main: Entering schedule to run every 30 minutes`);
-  // Schedule the transferBalances method to run every 30 minutes
+
+if (DEBUG) monitoring.debug(`Main: Setting up rpc client`);
+const rpc = new RpcClient({
+  resolver: new Resolver(),
+  encoding: Encoding.Borsh,
+  networkId: config.network,
+});
+  if (DEBUG) monitoring.debug(`Main: Starting rpc connection`);
+  await rpc.connect();
+  const serverInfo = await rpc.getServerInfo();
+  if (!serverInfo.isSynced || !serverInfo.hasUtxoIndex) throw Error('Provided node is either not synchronized or lacks the UTXO index.');
+  if (DEBUG) monitoring.debug(`Main: RPC connection established`);    
+
+if (DEBUG) monitoring.debug(`Main: Starting transactionManager`);
+const transactionManager = new trxManager(config.network, treasuryPrivateKey, databaseUrl, rpc);
+
+
 // Schedule the transferBalances method to run every 30 minutes
 cron.schedule('*/30 * * * *', async () => {
-  if (DEBUG) monitoring.debug(`Main: Setting up rpc client`);
-  const rpc = new RpcClient({
-    resolver: new Resolver(),
-    encoding: Encoding.Borsh,
-    networkId: config.network,
-  });
-    if (DEBUG) monitoring.debug(`Main: Starting rpc connection`);
-    await rpc.connect();
-    const serverInfo = await rpc.getServerInfo();
-    if (!serverInfo.isSynced || !serverInfo.hasUtxoIndex) throw Error('Provided node is either not synchronized or lacks the UTXO index.');
-    if (DEBUG) monitoring.debug(`Main: RPC connection established`);    
-
-  if (DEBUG) monitoring.debug(`Main: Starting transactionManager`);
-  const transactionManager = new trxManager(config.network, treasuryPrivateKey, databaseUrl, rpc);
-
   try {
-    await transactionManager.init();
     monitoring.log('Main: Running scheduled balance transfer');
     await transactionManager.transferBalances();
   } catch (transactionError) {
     monitoring.error(`Main: Transaction manager error: ${transactionError}`);
   }
-
-  if (DEBUG) monitoring.debug(`Main: Starting rpc disconnection`);
-  await rpc.disconnect();
 
 });
 monitoring.log('Main: Scheduled balance transfer every 30 minutes');
