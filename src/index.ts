ceeab1161d454ca3a75941868464c7dd993e5435
coinchimp <pinrojas+crypto@gmail.com>
2024-08-04 21:18:30 +0200

    moving schedule to happen every two hours

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+/**
+ * Made by coinchimp (Twitter: https://x.com/coinchimpx)
+ * 
+ * This script initializes the kaspool Payment App, sets up the necessary environment variables,
+ * and schedules a balance transfer task to run every 2 hours. It also provides progress logging 
+ * every 10 minutes.
+ */
+
 import { RpcClient, Encoding, Resolver } from "../wasm/kaspa";
 import config from "../config/config.json";
 import dotenv from 'dotenv';
@@ -5,78 +13,78 @@ import Monitoring from './monitoring';
 import trxManager from './trxs';
 import cron from 'node-cron';
 
+// Debug mode setting
 export let DEBUG = 0;
 if (process.env.DEBUG === "1") {
   DEBUG = 1;
 }
+
 const monitoring = new Monitoring();
 monitoring.log(`Main: Starting kaspool Payment App`);
 
 dotenv.config();
 
+// Environment variable checks
 const treasuryPrivateKey = process.env.TREASURY_PRIVATE_KEY;
 if (!treasuryPrivateKey) {
   throw new Error('Environment variable TREASURY_PRIVATE_KEY is not set.');
 }
-if (DEBUG) monitoring.debug(`Main: Getting private key`);
+if (DEBUG) monitoring.debug(`Main: Obtained treasury private key`);
 
 if (!config.network) {
   throw new Error('No network has been set in config.json');
 }
-
-/*if (!config.node) {
-  throw new Error('No node has been set in config.json');
-}*/
-
-if (DEBUG) monitoring.debug(`Main: Getting Network Id: ${config.network}`);
+if (DEBUG) monitoring.debug(`Main: Network Id: ${config.network}`);
 
 const kaspoolPshGw = process.env.PUSHGATEWAY;
 if (!kaspoolPshGw) {
   throw new Error('Environment variable PUSHGATEWAY is not set.');
 }
-if (DEBUG) monitoring.debug(`Main: Getting pushGateway`);
+if (DEBUG) monitoring.debug(`Main: PushGateway URL obtained`);
 
 const databaseUrl = process.env.DATABASE_URL;
 if (!databaseUrl) {
   throw new Error('Environment variable DATABASE_URL is not set.');
 }
-if (DEBUG) monitoring.debug(`Main: Getting Database URL`);
+if (DEBUG) monitoring.debug(`Main: Database URL obtained`);
 
-if (DEBUG) monitoring.debug(`Main: Entering schedule to run every 30 minutes`);
-
-if (DEBUG) monitoring.debug(`Main: Setting up rpc client`);
+// RPC client setup
+if (DEBUG) monitoring.debug(`Main: Setting up RPC client`);
 const rpc = new RpcClient({
   resolver: new Resolver(),
   encoding: Encoding.Borsh,
   networkId: config.network,
 });
-  if (DEBUG) monitoring.debug(`Main: Starting rpc connection`);
-  await rpc.connect();
-  const serverInfo = await rpc.getServerInfo();
-  if (!serverInfo.isSynced || !serverInfo.hasUtxoIndex) throw Error('Provided node is either not synchronized or lacks the UTXO index.');
-  if (DEBUG) monitoring.debug(`Main: RPC connection established`);    
 
-if (DEBUG) monitoring.debug(`Main: Starting transactionManager`);
+if (DEBUG) monitoring.debug(`Main: Starting RPC connection`);
+await rpc.connect();
+const serverInfo = await rpc.getServerInfo();
+if (!serverInfo.isSynced || !serverInfo.hasUtxoIndex) {
+  throw Error('Provided node is either not synchronized or lacks the UTXO index.');
+}
+if (DEBUG) monitoring.debug(`Main: RPC connection established`);
+
+// Transaction Manager setup
+if (DEBUG) monitoring.debug(`Main: Starting transaction manager`);
 const transactionManager = new trxManager(config.network, treasuryPrivateKey, databaseUrl, rpc);
 
-
-// Schedule the transferBalances method to run every 30 minutes
-cron.schedule('*/30 * * * *', async () => {
+// Schedule balance transfer every 2 hours
+cron.schedule('0 */2 * * *', async () => {
   try {
     monitoring.log('Main: Running scheduled balance transfer');
     await transactionManager.transferBalances();
   } catch (transactionError) {
     monitoring.error(`Main: Transaction manager error: ${transactionError}`);
   }
-
 });
-monitoring.log('Main: Scheduled balance transfer every 30 minutes');
 
-// Progress indicator logging every 5 minutes
+monitoring.log('Main: Scheduled balance transfer every 2 hours');
+
+// Progress indicator logging every 10 minutes
 setInterval(() => {
   const now = new Date();
   const minutes = now.getMinutes();
-  const remainingMinutes = 30 - (minutes % 30);
-  const remainingTime = remainingMinutes === 30 ? 0 : remainingMinutes;
+  const remainingMinutes = 120 - (minutes % 120);
+  const remainingTime = remainingMinutes === 120 ? 0 : remainingMinutes;
   if (DEBUG) monitoring.debug(`Main: ${remainingTime} minutes until the next balance transfer`);
-}, 5 * 60 * 1000); // 5 minutes in milliseconds
+}, 10 * 60 * 1000); // 10 minutes in milliseconds
